cea4e0529fedfd4612a7552e04ebaf4b1abdc1ae
volodkaorlov <volodka.orlov@gmail.com>
2020-05-17 04:22:33 +0300

    Added regexps

--- regexps.rkt
+++ regexps.rkt
@@ -1,6 +1,41 @@
 #lang racket
 
-(define (main-regex str)
-  (regexp-match #px"select (?:(distinct) (.*)|(MAX)\\((\\w*)\\)|(COUNT)\\((\\w*)\\)|(AVG)\\((\\w*)\\)|(.*)) (from (?:\\w*))(?:\\s)?(where\\([\\w\\d\\s=,]*\\)|orderby\\([\\w\\d=,]*\\))?(?:\\s)?(orderby\\([\\w\\d=,]*\\))?" str))
+(define (remove-false-from-list regex-list)
+  (remove (list-ref (remf* false? regex-list) 0) (remf* false? regex-list)))
 
-(main-regex "select AVG(row) from table where(row=1 and col=2) orderby(row,row)")
+(define (main-regex query)
+  (query-to-hash (remove-false-from-list (regexp-match #px"(select) (?:(distinct) (.*)|(MED)\\((\\w*)\\)|(COUNT)\\((\\w*)\\)|(SUM)\\((\\w*)\\)|(.*)) (from) (\\w*)(?:\\s)?(?:(where)\\(([\\w\\d\\s=,]*)\\)|(orderby)\\(([\\w\\d=,]*(?:\\sASC|\\sDESC)?)\\))?(?:\\s)?(?:(orderby)\\(([\\w\\d=,]*(?:\\sASC|\\sDESC)?)\\))?"
+                                                       query)) (make-hash)))
+
+(define (query-to-hash query query-hash)
+  (cond
+    [(empty? query) query-hash]
+    [(equal? (first query) "select")
+     (cond
+       ; Distinct
+       [(equal? (second query) "distinct")
+        (hash-set! query-hash "distinct" #t)
+        (hash-set! query-hash "select" (third query))
+        (query-to-hash (list-tail query 3) query-hash)]
+       
+       ; AGRF
+       [(regexp-match #px"(MED|COUNT|SUM)" (second query))
+        (hash-set! query-hash "agregate" (second query))
+        (hash-set! query-hash "select" (third query))
+        (query-to-hash (list-tail query 3) query-hash)]
+
+       ; Default
+       [else
+        (hash-set! query-hash "select" (second query))
+        (query-to-hash (list-tail query 2) query-hash)])]
+    ; From
+    [(equal? (first query) "from") (hash-set! query-hash "from" (second query)) (query-to-hash (list-tail query 2) query-hash)]
+    [else
+     (cond
+       ; Where
+       [(equal? (first query) "where") (hash-set! query-hash "where" (second query)) (query-to-hash (list-tail query 2) query-hash)]
+       ; Orderby
+       [(equal? (first query) "orderby") (hash-set! query-hash "orderby" (second query)) (query-to-hash (list-tail query 2) query-hash)])]))
+
+
+(main-regex "select MED(row) from table where(row=1 and col=2) orderby(row,col)")
